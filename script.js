74b410ce7599fa2dedb8c75ceaf88764a5b9ac6d
shevchenkotaras <shevchenkotv91@gmail.com>
2017-03-04 16:52:45 +0200

    custom service

--- script.js
+++ script.js
@@ -1,31 +1,51 @@
 // Code goes here
 var appModule = angular.module("githubViewer", []);
 
-var MainController = function($scope, $http){
+var MainController = function($scope, github, $interval, $log, $anchorScroll, $location){
 
-    var onUserComplete = function(response){
-        $scope.user = response.data;
-        $http.get($scope.user.repos_url)
-            .then(onRepos, onError)
+    var onUserComplete = function(data){
+        $scope.user = data;
+        github.getRepos($scope.user).then(onRepos, onError)
     };
 
-    var onRepos = function(response){
-        $scope.repos = response.data;
+    var onRepos = function(data){
+        $scope.repos = data;
+        $location.hash("UserDetails");
+        $anchorScroll();
     }
 
     var onError = function(reason){
         $scope.error = "Could not fetch the data";
     };
 
+    var decrementCountdown = function(){
+        $scope.countdown -= 1;
+        if($scope.countdown  < 1){
+            $scope.search($scope.username);
+        }
+    };
+
+
+    var countdownInterval = null;
+    var startCountdown = function(){
+        countdownInterval = $interval(decrementCountdown, 1000, $scope.countdown);
+    };
+
     $scope.search = function(username){
-        $http.get("https:/api.github.com/users/" + username)
-        .then(onUserComplete, onError);
+        $log.info("Searching fo " + username);
+        github.getUser(username).then(onUserComplete, onError);
+        if(countdownInterval){
+            $interval.cancel(countdownInterval);
+            $scope.countdown = null;
+        }
     };
     
 
     $scope.message = "GitHub Viewer";
     $scope.username = "angular";
     $scope.repoSortOrder = "-stargazers_count";
+    $scope.countdown = 5;
+    startCountdown();
 };
 
-appModule.controller("MainController", ["$scope", "$http", MainController]);
+appModule.controller("MainController", ["$scope", "github", "$interval", "$log", "$anchorScroll", "$location", MainController]);
